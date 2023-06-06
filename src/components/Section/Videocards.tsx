a306a54933cbb10e94ba27ce7439d572ebef6685
wooniwinnie <wooniwinnie@gmail.com>
2023-06-06 15:30:49 +0900

    배열 상태관리를 통해 비디오 카드 뽑아내보기

--- src/components/Section/Videocards.tsx
+++ src/components/Section/Videocards.tsx
@@ -48,28 +48,30 @@ type VideoItem = {
     };
 };
 
-const Videocards = () => {
-    const [videos, setVideos] = useState<VideoItem[]>([]);
+const Videocards = (): JSX.Element => {
+    const [data, setData] = useState<VideoItem[]>([]);
     useEffect(() => {
         fetch(`videos/popular.json`)
             .then((res) => res.json())
             .then((data) => {
-                const videoItem: VideoItem[] = data.items.map(
-                    (item: any) => item.snippet
-                );
-                setVideos(videoItem);
-                console.log(videoItem);
-                // const videoSnippets = videoItem.map((item) => item.snippet);
-                // setVideos(videoSnippets);
+                const dataItem = data.items;
+                setData(dataItem);
+            })
+            .catch((error) => {
+                console.log('에러가 발생했어요', error);
             });
     }, []);
+
     return (
         <div>
-            {videos.map((video) => (
-                <div key={uuidv4()}>
-                    <img src='' alt='' />
-                    <h2></h2>
-                    <p></p>
+            {data.map((item) => (
+                <div>
+                    <img
+                        src='https://images.unsplash.com/photo-1686007579079-550a74a0decb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60'
+                        alt=''
+                    />
+                    <h3>{item.snippet.title}</h3>
+                    <p>{item.snippet.channelTitle}</p>
                 </div>
             ))}
         </div>
